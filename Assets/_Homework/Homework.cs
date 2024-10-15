1516a46ec72766350f56127431d6bb1aadd01233
Nori <jiwonchichi1@gmail.com>
2024-10-15 16:54:56 +0900

    241015 16:54 과제 완료

---
+++ Assets/_Homework/Homework.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class MyObject
+{
+	public Color tColor;
+	public Renderer tRenderer;
+	public MeshFilter tMeshFilter;
+
+	public Vector3 tPosition;
+	public Mesh tMesh;
+	public Transform tTransform;
+}
+
+public class Homework : MonoBehaviour
+{
+	public List<MyObject> myObjects;
+	// Start is called before the first frame update
+	void Start()
+	{
+		foreach(MyObject obj in myObjects)
+		{
+			obj.tRenderer.material.color = obj.tColor;
+			obj.tMeshFilter.mesh = obj.tMesh;
+			obj.tTransform.position = obj.tPosition;
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
