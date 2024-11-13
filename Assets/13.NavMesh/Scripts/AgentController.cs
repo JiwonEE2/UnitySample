7db459c18f5ee7d48f89bd01c84511fa5e075de9
JiwonEE2 <jiwonchichi1@gmail.com>
2024-11-13 12:03:19 +0900

    241113 12:03 navMesh중..

---
+++ Assets/13.NavMesh/Scripts/AgentController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AgentController : MonoBehaviour
+{
+	public Transform pointer;
+	private NavMeshAgent agent;
+
+	private void Awake()
+	{
+		agent = GetComponent<NavMeshAgent>();
+	}
+
+	private void Update()
+	{
+		// AI에게 특정 지점으로 이동하도록 하는 함수
+		agent.SetDestination(pointer.position);
+	}
+}
