dbec490bde91510e2a5b01b48cb2f272b530f7cd
JiwonEE2 <jiwonchichi1@gmail.com>
2024-11-13 14:31:25 +0900

    241113 14:31 수업 끝

--- Assets/13.NavMesh/Scripts/Pointer.cs
+++ Assets/13.NavMesh/Scripts/Pointer.cs
@@ -9,14 +9,11 @@ public class Pointer : MonoBehaviour
 	public LayerMask targetLayer;
 	private Renderer childRenderer;
 	private Sequence jumpSequence;
+	private Tweener jumpTween;
 
 	private void Awake()
 	{
 		childRenderer = GetComponentInChildren<Renderer>();
-		transform.GetChild(0).DOLocalJump(Vector3.zero, 10f, 2, 0.5f)
-			.OnStart(() => childRenderer.enabled = true)
-			.OnComplete(() => childRenderer.enabled = false);
-
 	}
 
 	private void Update()
@@ -27,8 +24,14 @@ public class Pointer : MonoBehaviour
 			if (Physics.Raycast(ray, out RaycastHit hit, targetLayer))
 			{
 				transform.position = hit.point;
+				if (jumpTween != null)
+				{
+					jumpTween.Complete();
+				}
 				childRenderer.enabled = true;
-				jumpSequence.Play();
+				jumpTween = transform.GetChild(0)
+					.DOPunchPosition(Vector3.up * 2f, 0.5f)
+					.OnComplete(() => childRenderer.enabled = false).SetEase(Ease.InOutBack);
 			}
 		}
 	}
