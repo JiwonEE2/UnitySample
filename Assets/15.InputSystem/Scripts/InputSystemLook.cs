6f26cf516429f7f0d32bc1fb442f2dbfe796ff2d
JiwonEE2 <jiwonchichi1@gmail.com>
2024-11-15 11:52:08 +0900

    241115 11:52 mouse look 구현중(3)

--- Assets/15.InputSystem/Scripts/InputSystemLook.cs
+++ Assets/15.InputSystem/Scripts/InputSystemLook.cs
@@ -5,10 +5,18 @@ using UnityEngine.InputSystem;
 
 public class InputSystemLook : MonoBehaviour
 {
+	public Transform cameraRig;
+	public float mouseSensitivity;
+	private float rigAngle = 0f;
+
 	public void OnLook(InputValue value)
 	{
 		print($"OnLook 호출. 파라미터 : {value.Get<Vector2>()}");
 		if (SimpleMouseControl2.isFocusing == false) return;
 		// esc 누르거나 포커스 벗어나면 마우스 인식 안함
+
+		Vector2 mouseDelta = value.Get<Vector2>();
+		transform.Rotate(0, mouseDelta.x * mouseSensitivity * Time.deltaTime, 0);
+		rigAngle -= mouseDelta.y * mouseSensitivity * Time.deltaTime;
 	}
 }
