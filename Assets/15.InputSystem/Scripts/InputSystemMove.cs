77dbe13a4f4765c0a4011ca12fb84a25226575b9
JiwonEE2 <jiwonchichi1@gmail.com>
2024-11-15 11:46:18 +0900

    241115 11:46 플레이어 움직임 구현 완료. Look 부분 구현중

--- Assets/15.InputSystem/Scripts/InputSystemMove.cs
+++ Assets/15.InputSystem/Scripts/InputSystemMove.cs
@@ -3,18 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(CharacterController), typeof(Animator))]
 public class InputSystemMove : MonoBehaviour
 {
-	Vector2 moveDir;
+	CharacterController charCtrl;
+	Animator animator;
+
+	public float walkSpeed;
+	public float runSpeed;
+
+	Vector2 inputValue;
+
+	private void Awake()
+	{
+		charCtrl = GetComponent<CharacterController>();
+		animator = GetComponent<Animator>();
+	}
 
 	private void OnMove(InputValue value)
 	{
-		moveDir = value.Get<Vector2>();
-		print($"OnMove 호출 됨. 파라미터 : {moveDir}");
+		inputValue = value.Get<Vector2>();
+		print($"OnMove 호출 됨. 파라미터 : {inputValue}");
 	}
 
 	private void Update()
 	{
+		Vector3 inputMoveDir = new Vector3(inputValue.x, 0, inputValue.y) * walkSpeed;
+		Vector3 actualMoveDir = transform.TransformDirection(inputMoveDir);
 
+		charCtrl.Move(actualMoveDir * Time.deltaTime);
+
+		animator.SetFloat("Xdir", inputValue.x);
+		animator.SetFloat("Ydir", inputValue.y);
+		animator.SetFloat("Speed", inputValue.magnitude);
 	}
 }
