399c91e4e677a85a40a8878b4eccf1eb9b34120b
JiwonEE2 <jiwonchichi1@gmail.com>
2024-11-15 11:49:33 +0900

    241115 11:49 mouse look 구현중(2)

--- Assets/15.InputSystem/Scripts/InputSystemLook.cs
+++ Assets/15.InputSystem/Scripts/InputSystemLook.cs
@@ -1,8 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputSystemLook : MonoBehaviour
 {
-  
+	public void OnLook(InputValue value)
+	{
+		print($"OnLook 호출. 파라미터 : {value.Get<Vector2>()}");
+		if (SimpleMouseControl2.isFocusing == false) return;
+		// esc 누르거나 포커스 벗어나면 마우스 인식 안함
+	}
 }
