d5c32e405d9f852942e83050c1719d8fb5f9ccd1
JiwonEE2 <jiwonchichi1@gmail.com>
2024-12-16 14:21:09 +0900

    WebRequest 정리하며 수정

--- Assets/20.WebRequest/Scripts/WebRequestDotnet.cs
+++ Assets/20.WebRequest/Scripts/WebRequestDotnet.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+// HttpClient 사용을 위함
 using System.Net.Http;
+// Task 사용을 위함
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
@@ -47,19 +49,26 @@ public class WebRequestDotnet : MonoBehaviour
 		// HttpClient는 IDiposable? 을 상속받는다. 그래서 Dispose를 해야하는데,
 		// dispose 할 필요 없이 using 문을 사용하면 중괄호가 끝나는 시점에 자동으로 Dispose 처리된다.
 		// 아래의 GetTexture 함수의 using 문 참고
+
+		// 비동기적으로 GetTexture 메서드를 호출하여 이미지를 가져옴
 		await GetTexture();
 		print("GetTexture() 호출");
 	}
 
 	private async Task GetTexture()
 	{
+		// HttpClient를 사용하여 HTTP 요청을 보냄
 		using (HttpClient client = new HttpClient())
 		{
 			// await 키워드가 있으니 여기서 리턴이 반환될 때까지 비동기 상태로 대기
+			// URL로부터 바이트 배열을 비동기적으로 가져옴
 			byte[] response = await client.GetByteArrayAsync(imageURL);
 
+			// 새로운 Texture2D 객체 생성
 			Texture2D texture = new Texture2D(1, 1);
+			// 바이트 배열로부터 텍스처 로드
 			texture.LoadImage(response);
+			// UI RawImage에 텍스처 설정
 			targetRawImage.texture = texture; // 암시적 형변환
 
 			// sprite로 변환 알아서 해보기
--- Assets/20.WebRequest/Scripts/WebRequestTest.cs
+++ Assets/20.WebRequest/Scripts/WebRequestTest.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+// UnityWebRequest 사용을 위한 선언
 using UnityEngine.Networking;
 
 public class WebRequestTest : MonoBehaviour
@@ -18,26 +19,34 @@ public class WebRequestTest : MonoBehaviour
 	IEnumerator GetWebTexture(string url)
 	{
 		// http로 웹 요청(Request)을 보낼 객체 생성
+		// URL로부터 텍스처를 요청하는 UnityWebRequest 생성
 		UnityWebRequest www = UnityWebRequestTexture.GetTexture(url);
 
 		// 코루틴을 통해 웹으로부터 요청에 대한 응답(Response)를 받을 때까지 비동기로 대기하는 객체를 받아옴
+		// 비동기 요청을 보냄
 		UnityWebRequestAsyncOperation operation = www.SendWebRequest();
+		// 요청이 완료될 때까지 대기
 		yield return operation;
 
+		// 요청이 성공했는지 확인
 		if (www.result != UnityWebRequest.Result.Success)
 		{
 			// 웹 통신 문제 발생
 			print($"Http 통신 실패: {www.error}");
 		}
+		// 텍스쳐 다운로드 성공
 		else
 		{
-			// 텍스쳐 다운로드 성공
+			// 다운로드한 텍스처를 가져옴
 			Texture texture = ((DownloadHandlerTexture)www.downloadHandler).texture;
 			// 텍스쳐를 런타임에서 Sprite로 변환
+			// 텍스처로부터 스프라이트 생성
 			Sprite sprite = Sprite.Create((Texture2D)texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
 			// image의 sprite 교체
+			// UI Image에 스프라이트 설정
 			targetImage.sprite = sprite;
 
+			// UI RawImage에 텍스처 설정
 			targetRawImage.texture = texture;
 		}
 	}
